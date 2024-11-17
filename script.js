a5f742d98f05053d1616bd8a5c900570071b9d1a
Ebo-stays <ebostays@gmail.com>
2024-11-17 15:44:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Enter your login and password</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<style type="text/css">
+      body,td { font-family: verdana,arial; color: #333333; font-size: 1.00em; font-weight: normal; font-style: normal; }
+      body { text-align:center; background-color: #E6E6E6;  }
+    </style>
+    </head>
+    <body>
+<!-- 1466 fm=[] ajax=1 -->
+<!--form-start-->
+<div style="text-align: center;">
+<div style="box-sizing: border-box; display: inline-block; width: auto; max-width: 600px; background-color: white; border: 1px solid #000F91; border-radius: 5px; box-shadow: 0px 0px 8px #000F91; margin: 50px auto auto;" id="ap_form_box">
+<div style="background: #000F91; border-radius: 5px 5px 0px 0px; padding: 15px 25px; position: relative"><span style="font-family: verdana,arial; color: white; font-size: 1.00em; font-weight:bold; font-style:normal; text-transform: none" id="ap_form_head">Enter your login and password</span></div>
+<div style="background: white; padding: 15px" id="ap_style">
+<style type="text/css" scoped>
+	#ap_style td { text-align:left; vertical-align: baseline; font-family: verdana,arial; color: #333333; font-size: 1.00em; font-weight:normal; font-style:normal; text-transform: none; border: 0px; line-height: 1.2; }
+	#ap_style td a { color: blue; }
+	#ap_style input,select { border: 1px solid #CCCCCC; border-radius: 5px; color: #666666; display: inline-block; font-size: 1.00em; box-sizing: border-box; padding: 5px; }
+	#ap_style input[type="text"], input[type="password"], input[type="email"], input[type="number"], select { width: 100%; background-color: white; }
+	#ap_style input[type="button"], input[type="reset"], input[type="submit"] { height: auto; width: auto; cursor: pointer; box-shadow: 0px 0px 5px #000F91; background: buttonface; float: right; text-align:right; margin-top: 10px; margin-left:7px;}
+	#ap_style input[type="checkbox"], input[type="radio"] { height: 1.00em; width: 1.00em; }
+	#ap_style table.center { margin-left:auto; margin-right:auto; border: 0px; }
+	#ap_style .error { font-family: verdana,arial; color: red; font-size: 1.00em; padding-bottom:10px; }
+	#ap_style .error_note { display: block; font-family: verdana,arial; color: red; font-size: 0.85em; padding-top:3px; }
+	#ap_style .error_box { border: 1px solid red; }
+	.ap_footer { width:100% text-align: center; font-size:smaller; background: #000F91; color: white } .ap_footer a { color: white; font-style: italic; }
+	.ap_form_inp { vertical-align: top; }
+	* { target-new: tab ! important }
+</style>
+
+<form method="post" action="https://www.authpro.com/auth/EBO/" name="aform" >
+<input type="hidden" name="action" value="login">
+<input type="hidden" name="user" value="EBO">
+<input type="hidden" name="hide" value="O">
+<table id='ap_table_login' width='100%' class='center'>
+<tr style='display:none' id='ap_form_err'><td colspan='2' id='ap_form_errmes' style='padding-bottom:10px' class='error'></td></tr>
+<tr style='display:none' id='ap_form_inf'><td colspan='2' id='ap_form_infmes' style='padding-bottom:10px'></td></tr>
+<tr class='ap_form_inp'><td>Username:</td><td><input type="text" name="login" value="" autocorrect="off" autocapitalize="off" autocomplete="username"></td></tr>
+<tr class='ap_form_inp'><td>Password:</td><td><input type="password" name="password" autocomplete="current-password"></td></tr>
+<tr><td>&nbsp;</td><td align='left'><input type='checkbox' name='remember' value='1'> Remember me</td></tr>
+<tr id='ap_form_isb'><td>&nbsp;</td><td><input type="submit" id='ap_form_btn' value="Enter"></td></tr>
+
+<tr id='ap_form_space'><td colspan=2>&nbsp;</td></tr>
+<tr><td colspan='2' id='ap_form_lostmes'><a href="https://www.authpro.com/auth/EBO/?action=lost">Forgot your username or password?</a></td></tr>
+<tr><td colspan='2' id='ap_form_regmes'>Not a member yet? <a href="https://www.authpro.com/auth/EBO/?action=reg">Join now</a>.</td></tr>
+</table>
+</form>
+<script type="text/javascript" src="https://www.authpro.com/js/aform.js"></script>
+    
+</div></div></div>
+
+<!--form-end--><br/><br/><div style='text-align:center;font-size:14px;position:fixed;width:100%;padding:5px;bottom:0px;'><hr style='width:90%;height:1px' /><i><span style='white-space: pre;'>This page is hosted by <a href="https://www.authpro.com/"><b>AuthPro</b></a>.</span> <span style='white-space: pre;'><a href="https://www.authpro.com/">Get your own for free!</a></span>&nbsp;&nbsp;<a href="https://www.authpro.com/abuse.shtml?EBO">Report abuse</a></i></div>
+</body>
+</html>
